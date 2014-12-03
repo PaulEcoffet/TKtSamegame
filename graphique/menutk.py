a32f0c0ef1293df83c37d670acc67bf1537f2ce2
fryoyo <gouetfrancois@gmail.com>
2014-12-03 14:42:48 +0100

    board tkinter

--- graphique/menutk.py
+++ graphique/menutk.py
@@ -3,9 +3,10 @@ from game.samegame import SameGame
 
 class MenuFrame(Frame):
 
-    def __init__(self,master):
-        super().__init__(master)
-        quit_button = Button(self,text="QUITTER",command=master.destroy)
+    def __init__(self,interface):
+        self.interface = interface
+        super().__init__(interface.root)
+        quit_button = Button(self,text="QUITTER",command=interface.destroy)
         quit_button.pack(side=LEFT, fill=BOTH)
         new_game_button = Button(self,text="NEW GAME",command=self.new_game)
         new_game_button.pack(side=LEFT, fill=BOTH)
@@ -13,5 +14,8 @@ class MenuFrame(Frame):
     def new_game(self):
         game = SameGame()
         game.new_game(10,10,4)
+        self.switch_frame(BoardFrame,game)
+        
+        
         
         
