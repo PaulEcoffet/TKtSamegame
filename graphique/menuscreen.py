c725afca6294f7f24ea935698a0e6042e6e54898
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-12-03 14:19:02 +0100

    switching from pygame to tkinter

--- graphique/menuscreen.py
+++ graphique/menuscreen.py
@@ -2,20 +2,22 @@ import pygame
 import pygame.locals as pl
 
 from graphique.screen import Screen
+from game.samegame import SameGame
+
 
 class MenuScreen(Screen):
     def __init__(self, interface):
         self.interface = interface
         new_game_button = pygame.sprite.Sprite()
-        new_game_button.image = pygame.image.load("new_game.png").convert()
-        new_game_button.rect = new_game_button.image.get_rect(center=(300, 300))
+        new_game_button.image = pygame.image.load("graphique/img/new_game.png").convert()
+        new_game_button.rect = new_game_button.image.get_rect(center=(400, 300))
 
         quit_button_game = pygame.sprite.Sprite()
-        quit_button_game.image = pygame.image.load("quit.png").convert()
-        quit_button_game.rect = quit_button_game.image.get_rect(center=(100, 300))
+        quit_button_game.image = pygame.image.load("graphique/img/quit.png").convert()
+        quit_button_game.rect = quit_button_game.image.get_rect(top=new_game_button.rect.bottom)
 
         self.buttons = pygame.sprite.Group(new_game_button,quit_button_game)
-        self.clickable = [(new_game_button,self.interface.new_game),
+        self.clickable = [(new_game_button,self.new_game),
                      (quit_button_game,self.interface.quitter)]
 
     def draw(self):
@@ -23,3 +25,11 @@ class MenuScreen(Screen):
         screen = pygame.Surface((dim.width, dim.height))
         self.buttons.draw(screen)
         return screen
+
+    def new_game(self):
+        game = SameGame()
+        game.new_game(10,10,4)
+        self.interface.change_screen(BoardScreen, game)
+
+    def load_game(self):
+        self.interface.change_screen(LoadScreen)
--- graphique/pygameinterface.py
+++ graphique/pygameinterface.py
@@ -1,41 +1,9 @@
-import pygame
-import pygame.locals as pl
-
-from game.samegame import SameGame
-from graphique.menuscreen import MenuScreen
+import tkinter as tk
 
 class PygameInterface(object):
 
     def __init__(self):
-        pygame.init()
-        try:
-            self.window = pygame.display.set_mode((800, 600))
-            self.game = None
-            self.do_run = True
-            self.screen = MenuScreen(self)
-            self.run()
-        finally:
-            pygame.quit()
+        self.root = tk.Tk()
+        frame = tk.Frame(self.root)
 
-    def run(self):
-        while self.do_run:
-            self.do_events()
-            self.screen.update()
-            self.window.blit(self.screen.draw(), (0,0))
-            pygame.display.flip()
-
-    def do_events(self):
-        for event in pygame.event.get():
-            if event.type == pl.QUIT:
-                self.do_run = False
-            elif event.type == pl.MOUSEBUTTONUP and event.button == 1:
-                for c in self.screen.clickable:
-                    if c[0].rect.collidepoint(*event.pos):
-                        c[1]()
-
-    def quitter(self):
-        self.do_run =False
-
-    def new_game(self):
-        self.game = SameGame()
-        self.game.new_game(10,10,4)
+        self.root.mainloop()
