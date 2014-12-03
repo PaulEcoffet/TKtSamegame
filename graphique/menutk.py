1e26aa51c603f6e4236f45b09f115de1aa4a55d3
fryoyo <gouetfrancois@gmail.com>
2014-12-03 15:06:16 +0100

    load working

--- graphique/menutk.py
+++ graphique/menutk.py
@@ -1,6 +1,8 @@
-from tkinter import *
+from tkinter import * 
 from game.samegame import SameGame
-from graphique.boardtk import boardFrame
+from graphique.boardtk import BoardFrame
+import pickle
+import tkinter.filedialog
 
 class MenuFrame(Frame):
 
@@ -11,11 +13,19 @@ class MenuFrame(Frame):
         quit_button.pack(side=LEFT, fill=BOTH)
         new_game_button = Button(self,text="NEW GAME",command=self.new_game)
         new_game_button.pack(side=LEFT, fill=BOTH)
+        load_button = Button(self,text="NEW GAME",command=self.load_game)
 
     def new_game(self):
         game = SameGame()
         game.new_game(10,10,4)
         self.switch_frame(BoardFrame,game)
+
+    def load_game(self):
+        f = tkinter.filedialog.askopenfile(mode='rb',initialdir = '../saves/' )
+        game = pickle.load(f)
+        self.switch_frame(BoardFrame, game)
+
+
         
         
         
