1d2f33b7da71a5c9651b4c1fe218942a0187febb
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-12-10 13:53:03 +0100

    Game optimisation + bug correction and nice message

--- graphique/boardtk.py
+++ graphique/boardtk.py
@@ -1,49 +1,68 @@
 from tkinter import *
+from game.errors import InvalidCellError, NotEnoughCellsError
 
 
-        
+
 class BoardFrame(Frame):
-    
+
     def __init__(self, interface, game):
         super().__init__(interface.root)
-        self.possible = {"B":"graphique/img/ZB.gif","O":"graphique/img/ZC.gif","A":"graphique/img/ZG.gif",
-                                "M":"graphique/img/ZM.gif", "V":"graphique/img/ZY.gif",
-                                "R":"graphique/img/ZR.gif"}
-
-        b = self.possible
+        self.possible = {"B":PhotoImage(file="graphique/img/ZB.gif"),
+                         "O":PhotoImage(file="graphique/img/ZC.gif"),
+                         "A":PhotoImage(file="graphique/img/ZG.gif"),
+                         "M":PhotoImage(file="graphique/img/ZM.gif"),
+                         "V":PhotoImage(file="graphique/img/ZY.gif"),
+                         "R":PhotoImage(file="graphique/img/ZR.gif"),
+                         " ": PhotoImage(width=48, height=48)}
         self.grid = {}
         self.interface = interface
         self.game = game
+        self.buttons = []
         self.board = Frame(self, relief=SOLID, border=2)
         self.board.pack(side=TOP, padx=5, pady=5)
+        self.gen_buttons()
         self.dispBoard()
-        self.label = Label(self, text="SCORE :" + str(self.game.score))
-        self.label.pack()
+        self.score = Label(self, text="SCORE :" + str(self.game.score))
+        self.score.pack(side=TOP)
+        self.message = Label(self, text='', fg='red')
+        self.message.pack(side=TOP)
 
-        
-        
+
+
 
     def cb_cell(self,cell):
-        while self.game.can_play and self.game.not_finished:
+        try:
             self.game.click_on_cell(cell[1],cell[0])
-            self.dispBoard()
-        if self.game.won:
-            self.label['text'] = "FELICITATION SCORE:" + str(self.game.score)
+        except InvalidCellError as e:
+            self.message['text'] = str(e)
+        except  NotEnoughCellsError as e:
+            self.message['text'] = 'Pas assez de cases de même couleur'
         else:
-            self.label['text'] = "PARTIE FINIE SCORE:" + str(self.game.score)
-            
+            self.message['text'] = ''
+        self.dispBoard()
+        if self.game.won:
+            self.score['text'] = "FELICITATION SCORE:" + str(self.game.score)
+        elif not self.game.can_play:
+            self.score['text'] = "PARTIE FINIE SCORE:" + str(self.game.score)
+        else:
+            self.score['text'] = 'SCORE: ' + str(self.game.score)
+
 
     def dispBoard(self):
         for loop in range(self.game.nb_col*self.game.nb_line):
             col = loop % self.game.nb_col
             line = loop // self.game.nb_line
-            if self.game.board[line][col] == ' ':
-                img = PhotoImage(width=50,height=50)
-            else:
-                img = PhotoImage(file= self.possible[self.game.board[line][col]])
-                print(img.width())
-            c = Button(self.board,image = img,command=lambda cell=(col,line): self.cb_cell(cell))
-            c.image = img
-            c.grid(column=col, row=line, ipadx=5, ipady=5)
+            self.buttons[line][col]['image'] = self.possible[self.game.board[line][col]]
 
-    
+    def gen_buttons(self):
+        if self.buttons:  # S'il y a des boutons, on nettoie
+            for line in self.buttons:
+                for button in line:
+                    button.destroy()
+        self.buttons = []
+        for line in range(self.game.nb_line):
+            self.buttons.append([])
+            for col in range(self.game.nb_col):
+                c = Button(self.board,command=lambda cell=(col,line): self.cb_cell(cell))
+                self.buttons[line].append(c)
+                c.grid(column=col, row=line)
